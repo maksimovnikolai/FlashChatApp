bd7072cfef3626c16d00443e2920580b22ddd4b7
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-26 19:58:09 +0300

    setup project

---
+++ FlashChatApp/Controllers/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  FlashChatApp
+//
+//  Created by Nikolai Maksimov on 26.02.2024.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
