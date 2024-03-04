72251a0e0cb685e4e32707e5e1de1258c135015d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-03-04 23:21:55 +0300

    add custom cell to chat vc

---
+++ FlashChatApp/ChatModule/ChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChatTableViewCell.swift
+//  FlashChatApp
+//
+//  Created by Nikolai Maksimov on 04.03.2024.
+//
+
+import UIKit
+
+final class ChatTableViewCell: UITableViewCell {
+    
+    static let identifier = "ChatTableViewCell"
+    
+}
